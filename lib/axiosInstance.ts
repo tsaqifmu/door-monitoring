2b568315d08f59d59e8d8f97bae8d058ea122b9f
Tsaqif Mushoddaq <mushoddaqt@gmail.com>
2024-06-18 14:13:29 +0700

    style: Update SyncLoader color in DashboardLayout component

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://s1.azizfath.com:3000",
+  baseURL: "http://s2.azizfath.com:3000",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
