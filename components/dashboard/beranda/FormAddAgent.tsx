da952119ecb15a26047e7beb20225f59f843455e
tsaqifmu <mushoddaqt@gmail.com>
2024-08-12 13:51:15 +0700

    fix: fixing problem conectivity, and add new environment variable

--- components/dashboard/beranda/FormAddAgent.tsx
+++ components/dashboard/beranda/FormAddAgent.tsx
@@ -31,12 +31,14 @@ const FormAddAgent = ({ setAddAgentOpen, refetchAgents }: any) => {
   });
 
   useEffect(() => {
-    const client = mqtt.connect("ws://10.1.1.103:1884", {
-      username: "aziz",
-      password: "1234",
+    const client = mqtt.connect(process.env.NEXT_PUBLIC_MQTT_URL!, {
+      username: process.env.NEXT_PUBLIC_MQTT_USERNAME,
+      password: process.env.NEXT_PUBLIC_MQTT_PASSWORD,
     });
+
     client.on("connect", () => {
       client.subscribe("rfid");
+      console.log("connected");
     });
 
     client.on("message", (topic, message) => {
--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosInstance } from "axios";
 
 // http://10.1.1.103:3000
+// http://baru.azizfath.com:3000
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://10.1.1.103:3000",
+  baseURL: "process.env.NEXT_PUBLIC_API_BASE_URL",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
