cbb989dc8c4f24c711a1f58cf10687560b3f1610
tsaqifmu <mushoddaqt@gmail.com>
2024-08-14 10:29:10 +0700

    feat: add new generate file ino

--- lib/generateFileContent.ts
+++ lib/generateFileContent.ts
@@ -4,27 +4,33 @@ const { randomUUID } = new ShortUniqueId({ length: 10 });
 
 export const generateFileContent = (data: any) => {
   return `
+  
   #include <SPI.h>
   #include <Ethernet.h>
   #include <PubSubClient.h>
   #include <ArduinoJson.h>
   #include <Wire.h>
-  
+  #include <iostream>
+  using namespace std;
+
   #define RFID Serial2
   #define RXD2 16
   #define TXD2 14
-  #define relay1 27
-  #define button1 25
+  #define relay1 26
+  #define button1 27
+  #define red 23
+  #define green 22
+  #define buzzerpin 21
   
   byte mac[] = {0x02, 0xAD, 0xBB, 0xCD, 0xDE, 0x01};
   EthernetClient client;
   PubSubClient mqttClient(client);
   
   //ALL MQTT VARIABLE
-  const char * mqtt_server = "s1.azizfath.com";
+  const char * mqtt_server = "10.1.1.103";
   const int mqtt_port = 1883;
   const char * mqtt_user = "aziz";
-  const char * mqtt_pass = "mqtt";
+  const char * mqtt_pass = "1234";
   const char * deviceId = "${randomUUID()}";
   const char * doorNumber = "${data}";
   const char * auth_topic = "auth";
@@ -34,21 +40,20 @@ export const generateFileContent = (data: any) => {
   long millis1 = 0;
   
   // STATIC IP
-  // #define MYIPADDR 10,1,1,129
-  // #define MYIPMASK 255,255,255,0
-  // #define MYGW 10,1,1,1
-  // #define MYDNS 10,1,1,1
+  #define MYIPADDR 10,62,1,252
+  #define MYIPMASK 255,255,255,0
+  #define MYGW 10,62,1,254
+  #define MYDNS 10,1,1,111
   
   StaticJsonDocument<200> doc1;
   StaticJsonDocument<200> doc2;
   
   char c;
-  String rfid_tag;
+  String rfid_tag, last_tag;
   String masterCardNumber = "310059E5A825";
   
-  int latestConditionInt;
+  int latestConditionInt, targetStatusInt, authStatus;
   String targetStatus;
-  int targetStatusInt;
   
   void setup() {
     pinMode(relay1,OUTPUT);
@@ -63,7 +68,6 @@ export const generateFileContent = (data: any) => {
     if (Ethernet.begin(mac)){
       Serial.println("DHCP OK!");
     } else {
-      // Serial.println("Failed to configure Ethernet using DHCP");
       if (Ethernet.hardwareStatus() == EthernetNoHardware) {
         Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
         while (true) continue;
@@ -99,36 +103,45 @@ export const generateFileContent = (data: any) => {
   }
   
   void mqttReconnect() {
-    while (!mqttClient.connected()) {
-    Serial.println("MQTT connection ...");
-      if (mqttClient.connect(deviceId, mqtt_user, mqtt_pass)) {
+    if (!mqttClient.connected()) {
+      Serial.println("MQTT connection ...");
+      
+      int randNum = random(100000);
+      char buffer[6];
+      sprintf(buffer, "%d", randNum);
+      char * devId = buffer;
+
+      if (mqttClient.connect(devId, mqtt_user, mqtt_pass)) {
         Serial.println("MQTT Connected");
         mqttClient.subscribe(door_topic);
+        Serial.print("door_topic: ");
+        Serial.println(door_topic);
       } else {
         Serial.print("failed, rc= ");
         Serial.print(mqttClient.state());
-        Serial.print(" try again in 5 seconds");
-        delay(5000);
+        Serial.println(" try again ...");
+        delay(100);
       }
     }
   }
-  
+
+
   void mqttCallback(char * topic, byte * payload, unsigned int length) {
     if (String(topic) == door_topic) {
       deserializeJson(doc1, payload, length);
-      
-      // Serial.println("pretty here:");
-      // serializeJsonPretty(doc1, Serial);
-      // Serial.println();
-      int authStatus = doc1["authStatus"];
+
+      authStatus = doc1["authStatus"];
       String targetStatus = doc1["targetStatusBool"];
-      int targetStatusInt = targetStatus.toInt();
-  
+      targetStatusInt = targetStatus.toInt();
+
       if(authStatus == 1) {
-        digitalWrite(relay1,targetStatusInt);
-        latestConditionInt=digitalRead(relay1);
+          Serial.println("Access accepted from database");
+          digitalWrite(relay1,targetStatusInt);
+          latestConditionInt=digitalRead(relay1);
+      } else if (authStatus == 0) {
+        Serial.println("Access denied");
+        last_tag="";
       }
-  
     }
   
   }
@@ -145,6 +158,8 @@ export const generateFileContent = (data: any) => {
       program();
     }
     
+
+
     while (RFID.available() > 0) {
       delay(5);
       c = RFID.read();
@@ -159,9 +174,9 @@ export const generateFileContent = (data: any) => {
   void program(){ 
     if (digitalRead(button1)==0){
       if (latestConditionInt==0){
-        digitalWrite(relay1,1);
+        relayOFF();
       } else if (latestConditionInt==1){
-        digitalWrite(relay1,0);
+        relayON();
       } 
       latestConditionInt=digitalRead(relay1);
     }
@@ -178,11 +193,12 @@ export const generateFileContent = (data: any) => {
     }
   }
   
-  String last_tag;
+  
   
   void check()
   {
     rfid_tag = rfid_tag.substring(1, 13);
+
     if (rfid_tag==last_tag) {
       Serial.println("same tag");
       last_tag="";
@@ -193,36 +209,40 @@ export const generateFileContent = (data: any) => {
     }
     
     last_tag=rfid_tag;
+
     Serial.println("Card ID : " + rfid_tag);
-    // Serial.println("Access ID : " + masterCardNumber);
-  
-    doc2["rfid"]=rfid_tag;
-    doc2["doorNumber"]=doorNumber;
-    doc2["deviceId"]=deviceId;
-    doc2["targetStatus"]=targetStatus;
-    doc2["targetStatusBool"]=String(targetStatusInt);
-  
-    String jsonString;
-    serializeJson(doc2,jsonString);
-  
-    // mqttClient.publish(auth_topic, (char * ) jsonString.c_str());
-  
-    if (masterCardNumber.indexOf(rfid_tag) >= 0) {
-      Serial.println("Access accepted");
-      if(latestConditionInt==1){
-        relayON();
-      } else relayOFF();
-      last_tag="";
+
+    if(!mqttClient.connected()){
+      if (masterCardNumber.indexOf(rfid_tag) >= 0) {
+        Serial.println("Access accepted from master card");
+        if(latestConditionInt==1){
+          relayON();
+        } else relayOFF();
+      } else {
+        Serial.println("Access denied");
+        last_tag="";
+      }
+      
     }
+
     else {
-      Serial.println("Access denied");
-      last_tag="";
+      
+      doc2["rfid"]=rfid_tag;
+      doc2["doorNumber"]=doorNumber;
+      doc2["deviceId"]=deviceId;
+      doc2["targetStatus"]=targetStatus;
+      doc2["targetStatusBool"]=String(targetStatusInt);
+    
+      String jsonString;
+      serializeJson(doc2,jsonString);
+    
+      mqttClient.publish(auth_topic, (char * ) jsonString.c_str());
     }
-  
-    delay(3000);
+
+    delay(1000);
     Serial.println(" ");
     Serial.println("Bring your RFID card closer …");
-    
+
   }
   
   void relayON(){
@@ -231,5 +251,21 @@ export const generateFileContent = (data: any) => {
   
   void relayOFF(){
     digitalWrite(relay1,1);
+  }
+
+  void lampRed(const String act){
+    if (act=="ON"){
+      digitalWrite(red,1);
+    } else if (act=="OFF"){
+      digitalWrite(red,0);
+    }
+  }
+
+  void lampGreen(const String act){
+    if (act=="ON"){
+      digitalWrite(green,1);
+    } else if (act=="OFF"){
+      digitalWrite(green,0);
+    }
   }`;
 };
